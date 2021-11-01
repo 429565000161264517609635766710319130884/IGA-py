3be4ee3500c86a5444864ebe7fe9dee26faf47a6
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2021-11-01 01:25:40 +0100

    tracking files.

---
+++ tests.py
@@ -0,0 +1,14 @@
+import src.core as Instagram
+
+
+def main():
+    target = "github"
+    user = Instagram.fetch_user(target)["graphql"]["user"]
+    print("The %s biography is '%s'." % (target, user["biography"].replace('\n', '\\n')))
+    print("It follows %d people and is followed by %d people." % (user["edge_followed_by"]["count"], user["edge_follow"]["count"]))
+    print("Also, this account %s a business one." % ("is" if user["is_business_account"] else "is not"))
+    print("Finally, this account %s verified." % ("is" if user["is_verified"] else "is not"))
+
+
+if __name__ == "__main__":
+    main()
